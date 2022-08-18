2b77bf1cc1d08bb633e093e817baa7a64e5302ca
Diego Zamparo da Silva <diego.silva@grupoboticario.com.br>
2022-08-18 11:28:30 -0300

    added broadcast

--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -15,7 +15,8 @@ type SingInCredentials = {
 };
 
 type AuthContextData = {
-  signIn(credentials: SingInCredentials): Promise<void>;
+  signIn: (credentials: SingInCredentials) => Promise<void>;
+  signOut: () => void;
   user: User;
   isAuthenticated: boolean;
 };
@@ -26,9 +27,13 @@ type AuthProviderProps = {
 
 export const AuthContext = createContext({} as AuthContextData);
 
-export function signOut() {
+let authChannel: BroadcastChannel;
+
+export function signOut(broadcast: boolean = true) {
   destroyCookie(undefined, "nextauth.token");
   destroyCookie(undefined, "nextauth.refreshToken");
+
+  if (broadcast) authChannel.postMessage("singOut");
 
   Router.push("/");
 }
@@ -36,6 +41,26 @@ export function signOut() {
 export function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<User>();
   const isAuthenticated = !!user;
+
+  useEffect(() => {
+    authChannel = new BroadcastChannel("auth");
+    authChannel.onmessage = (message) => {
+      switch (message.data) {
+        case "singOut":
+          signOut(false);
+          break;
+        case "signIn":
+          const { "nextauth.token": token } = parseCookies();
+
+          if (token) {
+            Router.push("/dashboard");
+          }
+          break;
+        default:
+          break;
+      }
+    };
+  }, []);
 
   useEffect(() => {
     const { "nextauth.token": token } = parseCookies();
@@ -65,7 +90,7 @@ export function AuthProvider({ children }: AuthProviderProps) {
         password,
       });
 
-      const { token, refreshToken, permission, roles } = response.data;
+      const { token, refreshToken, permissions, roles } = response.data;
 
       setCookie(undefined, "nextauth.token", token, {
         maxAge: 60 * 60 * 24 * 30, // 30 days
@@ -78,11 +103,13 @@ export function AuthProvider({ children }: AuthProviderProps) {
 
       setUser({
         email,
-        permissions: permission,
+        permissions,
         roles,
       });
 
       api.defaults.headers["Authorization"] = `Bearer ${token}`;
+
+      authChannel.postMessage("signIn");
 
       Router.push("/dashboard");
     } catch (err) {
@@ -91,7 +118,7 @@ export function AuthProvider({ children }: AuthProviderProps) {
   }
 
   return (
-    <AuthContext.Provider value={{ signIn, isAuthenticated, user }}>
+    <AuthContext.Provider value={{ signIn, signOut, isAuthenticated, user }}>
       {children}
     </AuthContext.Provider>
   );
